d3d4b783b0934ba897913653a3ec148929fae013
TanyaChichkan <tchichkan28@gmail.com>
2021-04-02 22:57:46 +0300

    finished with problems

--- app.js
+++ app.js
@@ -24,12 +24,11 @@ const runServer = async () => {
       useFindAndModify: false,
     });
     console.log('Database connection is successful');
+    app.use(express.static('public'));
 
     app.use(logger(formatsLogger));
     app.use(cors());
     app.use(express.json());
-
-    app.use(express.static('/public'));
 
     app.use('/auth', authRouter);
     app.use('/api/contacts', contactsRouter);
--- auth/createAvatarfile.js
+++ auth/createAvatarfile.js
@@ -2,25 +2,21 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 const app = require('../app');
-const saveDir = path.join(process.cwd(), 'public');
+const IMG_DIR = path.join(process.cwd(), 'public', 'images');
+const uploadDir = path.join(process.cwd(), 'tmp');
 
 const avatarCreator = (url, userId) => {
   try {
     const fileName = `${userId}.jpg`;
-    const uploadDir = path.join(process.cwd(), 'temp');
-
     const file = fs.createWriteStream(`tmp/${fileName}`);
     const request = https.get(url, function (response) {
       response.pipe(file);
     });
 
-    console.log(file);
-
-    // moveAvatar(file);
+    moveAvatar(file, fileName);
 
     const avatarURL = changeAvatarURL(fileName);
     return avatarURL;
-
   } catch (err) {
     console.log(err);
   }
@@ -28,23 +24,26 @@ const avatarCreator = (url, userId) => {
 
 const moveAvatar = async (file, fileName) => {
   try {
-    const IMG_DIR = path.join(process.cwd(), 'public', 'images');
-    await fs.promises.rename(file.path, path.join(IMG_DIR, `${fileName}`), function (err) {
-      if (err) {
-        throw err;
+    await fs.promises.rename(
+      path.join(uploadDir, fileName),
+      path.join(IMG_DIR, fileName),
+      function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log("Moved to 'public'");
       }
-      console.log("Moved 'foo.txt' to 'bar.txt'");
-    });
+    );
   } catch (err) {
     console.log(err);
   }
 };
 
-const changeAvatarURL=(fileName)=>{
-    return `http://locahost:3000/images/${fileName}`
-}
+const changeAvatarURL = (fileName) => {
+  return `http://locahost:3000/images/${fileName}`;
+};
 
 module.exports = {
   avatarCreator,
-  changeAvatarURL
+  changeAvatarURL,
 };
